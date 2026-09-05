fbde9ec2448eb5324691aaea2d2f6009a6e5e61c
Alessandro Barbagallo <alessbarb@gmail.com>
2026-09-05 16:43:55 +0200

    style: satisfy migration lint rules

--- custom_components/bindhome/migrations.py
+++ custom_components/bindhome/migrations.py
@@ -2,9 +2,10 @@
 
 from __future__ import annotations
 
+from collections.abc import Callable
 from copy import deepcopy
 from dataclasses import dataclass
-from typing import Any, Callable
+from typing import Any
 
 from .const import REGISTRY_SCHEMA_VERSION
 from .registry import BindHomeRegistry, RegistryValidationError
